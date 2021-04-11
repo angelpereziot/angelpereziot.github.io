80706277f57dfdb4a4d1b34b82fc51e444a36fe4
angelpereziot <82333575+angelpereziot@users.noreply.github.com>
2021-04-11 18:04:34 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,7 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       `&copy; 2021
-      Gilberto Pacheco Gallegos.`;
+      ANGEL DANIEL PEREZ.`;
   }
 }
 customElements.define(
