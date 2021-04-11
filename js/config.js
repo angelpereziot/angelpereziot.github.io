a0b8c9c72440e437bcb1639df70ba8b25b2a1cbb
angelpereziot <82333575+angelpereziot@users.noreply.github.com>
2021-04-11 15:24:41 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,7 +11,7 @@ class MiNav extends HTMLElement {
         </span>
         Inicio
       </a>
-      <a href="formulario.html">
+      <a href="dispositivo.html">
         <span class="material-icons">
           smartphone
           </span>
