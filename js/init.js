ad59aeaabfb3b0a4054d1fc8f2c61ab4f7e2cb22
angelpereziot <82333575+angelpereziot@users.noreply.github.com>
2021-04-11 12:56:35 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyDr4xlb0Jd5-Pp2hcaCw2UUaZvoC2UmaLw",
+    authDomain: "angelpereziot-b1ce5.firebaseapp.com",
+    projectId: "angelpereziot-b1ce5",
+    storageBucket: "angelpereziot-b1ce5.appspot.com",
+    messagingSenderId: "996598139385",
+    appId: "1:996598139385:web:5c0d4ae0f69a78be161528"
 });
